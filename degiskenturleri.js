bc6b45149e04fa74b746d12661b4ebf98fe49d66
kaancankat <kaancankat1@gmail.com>
2023-01-09 23:29:38 +0300

    update

---
+++ degiskenturleri.js
@@ -0,0 +1,34 @@
+var urunAdi = "iphone 11" //string
+let urunFiyat = 18000   //number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+
+let a = 10
+let b = 20
+
+// console.log(Number(a) + Number(b)); "10" gibi ifadeleri numbera dönüştürmek gerekir
+
+console.log(a.toString() + b.toString());
+
+let isim = "kaan";
+let soyad = "cankat"
+console.log((isim) + " " +  (soyad));
+
+let sinavNotu = 12;
+
+let basarilimi = (sinavNotu >= 50)
+
+console.log(basarilimi);
+console.log(typeof basarilimi); //boolean
+
+let yas;
+
+
+yas = "";
+
+console.log(yas);
+console.log(typeof yas);
+//7. bölümdesin
+
---
+++ javascriptdegisken1.js
@@ -0,0 +1,12 @@
+var maasAli = 7000;
+var maascan = 6000;
+
+var zam = 0.35
+
+console.log(maasAli + (maasAli * zam))
+console.log(maascan + (maascan * zam))
+
+//türkçe karakter kullaılamaz 
+//arada boşluk olmaz
+//sayı ile başlamaz
+
