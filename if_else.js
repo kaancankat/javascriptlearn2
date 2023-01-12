b437678a52b02e5529e357109e6c59fc8b2448b0
kaancankat <kaancankat1@gmail.com>
2023-01-12 23:36:10 +0300

    update

---
+++ if_else.js
@@ -0,0 +1,13 @@
+let username = "kaancankat";
+let pasword = "1234";
+
+if (username == "kaancankat"){  
+       if (pasword == "1234")
+        
+    console.log("uygulamaya giriş yaptin");
+        else 
+        console.log("şifren yanliş");
+    }
+           else {
+    console.log("kullanici adin yanliş");
+}
---
+++ ifelse2.js
@@ -0,0 +1,9 @@
+let yas = 19
+let mezuniyet = "lise"
+
+if ((yas >= 18) && (mezuniyet == "lise") ) {
+    console.log("ehliyet alabilir");
+}
+else{
+    console.log("ehliyet alamaz")
+}
