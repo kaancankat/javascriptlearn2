44f5e5241b9cbcc32bf408ebcaeca4e02e15e8cb
kaancankat <kaancankat1@gmail.com>
2023-01-16 23:03:46 +0300

    update

---
+++ stringmetot.js
@@ -0,0 +1,17 @@
+let kursadi = " komple uygulamayi wep geliştirme eğitimi.";
+
+let sonuc;
+
+ sonuc = kursadi.toLowerCase();
+ sonuc = kursadi.toUpperCase();
+ sonuc = kursadi.length;
+ sonuc = kursadi.slice(0, 6);
+ sonuc = kursadi.slice(-10);
+
+ sonuc = kursadi.substring(0, 6);
+ sonuc = kursadi.replace("eğitimi" , "kursu");
+ sonuc = kursadi.trim();
+ sonuc = kursadi.indexOf("komple")
+ sonuc = kursadi.split("")
+
+console.log(sonuc);
