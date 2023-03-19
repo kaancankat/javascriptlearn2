c6a5dd837f9a299b2c406b635f442994e7248aff
kaancankat <kaancankat1@gmail.com>
2023-03-19 21:51:57 +0300

    update

--- sinama bilet/script.js
+++ sinama bilet/script.js
@@ -1,52 +1,73 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const repassword = document.getElementById('repassword');
+const container = document.querySelector('.container');
+const count = document.getElementById('count');
+const amount = document.getElementById('amount');
+const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
 
-function error(input, message) {
-    input.className = 'form-control is-invalid';
-    const div = input.nextElementSibling;
-    div.innerText = message;
-    div.className = 'invalid-feedback';
-}
+getFromLocalStorage();
+calculateTotal();
 
-function success(input) {
-    input.className = 'form-control is-valid';
-}
-
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    if(username.value === '') {
-        error(username, 'username gerekli');
-    }else {
-        success(username);
-    }
- 
-    if(email.value === '') {
-        error(email, 'email gerekli');
-    }else if(!validateEmail(email.value)) {
-        error(email, 'düzgün bir mail adresi giriniz');
-    }    
-    else {
-        success(email);
-    }
-
-    if(password.value === '') {
-        error(password, 'password gerekli');
-    }else {
-        success(password);
-    }
-
-    if(repassword.value === '') {
-        error(repassword,'repassword gerekli' );
-    }else {
-        success(repassword);
+container.addEventListener('click', function(e) {
+   if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
+       e.target.classList.toggle('selected');
+       calculateTotal()      
     }
 });
+
+select.addEventListener('change', function(e) {
+    calculateTotal();  
+});
+
+function calculateTotal() {
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat) {
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat) {
+        seatsArr.push(seat);
+    });
+
+    // [1,3,5]
+    let selectedSeatIndexs = selectedSeatsArr.map(function(seat) {
+        return seatsArr.indexOf(seat);
+    });
+
+    let selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocalStorage(selectedSeatIndexs);
+}
+
+function getFromLocalStorage() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !=null && selectedSeats.length > 0) {
+        seats.forEach(function(seat, index) {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex != null) {
+        select.selectedIndex = selectedMovieIndex;
+    }
+}
+
+function saveToLocalStorage(indexs) {
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
+}
+
+
+
